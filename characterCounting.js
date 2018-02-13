53e564c0fa1f50072e5bfcac8289d1b090f94ce1
Harry <harrylll93@gmail.com>
2018-02-13 17:05:03 +0000

    initial commit

---
+++ characterCounting.js
@@ -0,0 +1,16 @@
+function countLetters (string) {
+  let word = string.split(" ").join("");
+
+  let letterCount = new Object();
+
+  for (let i = 0; i < word.length; i++) {
+    if(!letterCount[word[i]]) {
+      letterCount[word[i]] = 0;
+    }
+    letterCount[word[i]]++;
+  }
+
+  return letterCount;
+}
+
+console.log(countLetters("lighthouse in the house"))
